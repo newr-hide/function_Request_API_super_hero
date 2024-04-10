3df91ee87670cbef89f6ae09c01ae9bb180ac0f4
Bondarev Artem <bond210484@mail.ru>
2024-04-10 21:49:26 +0700

    add file 1 part

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
+from pprint import pprint
 
+def get_the_smartest_superhero() -> str:
+    the_smartest_superhero = ''
+    url = "https://akabab.github.io/superhero-api/api"
+    response = requests.get(f'{url}/all.json')
+    pprint(response.json())
+
+    with open('image_name.json', "w") as file:
+        file.write(response.text)
+    return the_smartest_superhero
+
+
+print(get_the_smartest_superhero())
